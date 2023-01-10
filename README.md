2f12bcaed24a89ee0b6fed68285ac7aedb090466
Peter Krysinski <pakrysin@gmail.com>
2023-01-10 10:09:53 -0700

    Updating README file with notes.  Correcting some typos on index.js.  Next, going to be working on the HTML build.

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ GIVEN a command-line application that accepts user input...
 - - - - -
 WHEN I am prompted for my team members and their information,
 THEN an HTML file is generated that displays a nicely formatted team roster based on user input.
->
+>IN-PROGRESS
 
 - - - - -
 WHEN I click on an email address in the HTML,
@@ -57,15 +57,15 @@ THEN I am presented with a menu with the option to add an engineer or an intern 
 - - - - -
 WHEN I select the engineer option,
 THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu.
->
+>Done.  Function added and tested for addEngineer().
 
 - - - - -
 WHEN I select the intern option,
 THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu.
->
+>Done.  Function added and tested for addIntern().
 
 - - - - -
 WHEN I decide to finish building my team,
 THEN I exit the application, and the HTML is generated.
->
+>IN-PROGRESS
 
--- index.js
+++ index.js
@@ -39,16 +39,111 @@ const startTeam = () => {
         console.log(`mgrName: ${response.mgrName} \nmgrEmployeeID: ${response.mgrEmployeeID} \nmgrEmailAddress: ${response.mgrEmailAddress} \nmgrOfficeNbr: ${response.mgrOfficeNbr}`)
         // END TEST CODE                    
         if (response.addTeamMember === 'Engineer') {
-        // call addEngineer()
+          addEngineer();
       } else if (response.addTeamMember === 'Intern') {
-        // call addIntern()
+          addIntern();
       } else {
         // Build HTML with objects created during inquirer prompts
         // call buildHTML()
-
       };
 
     });
+};
+
+const addEngineer = () => {
+  inquirer
+  .prompt([
+    {
+    type: 'input',
+    message: 'Please enter the name of the engineer being entered!',
+    name: 'engName',
+    },
+    {
+    type: 'input',
+    message: 'What is your employee ID?',
+    name: 'engEmployeeID',
+    },
+    {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'engEmailAddress',
+    },
+    {
+    type: 'input',
+    message: 'What is your github user name?',
+    name: 'engGitHub',
+    },
+    {
+    type: 'list',
+    message: 'Would you like to add any team members, or save your team as-is?',
+    name: 'addTeamMember',
+    choices: ['Engineer', 'Intern', 'Save Team'],
+    },
+  ])
+  // Once user prompts have been completed, ask the user if they're done creating their team, or if they want to add more memebers
+  .then((response) => {
+      // START TEST CODE
+      console.log("Engineer added!");
+      console.log(`engName: ${response.engName} \nengEmployeeID: ${response.engEmployeeID} \nengEmailAddress: ${response.engEmailAddress} \nmgrengGitHub: ${response.engGitHub}`)
+      // END TEST CODE 
+      if (response.addTeamMember === 'Engineer') {
+        addEngineer();
+    } else if (response.addTeamMember === 'Intern') {
+        addIntern();
+    } else {
+      // Build HTML with objects created during inquirer prompts
+      // call buildHTML()
+    };
+
+  });
+};
+
+const addIntern = () => {
+  inquirer
+  .prompt([
+    {
+    type: 'input',
+    message: 'Please enter the name of the intern being entered!',
+    name: 'intName',
+    },
+    {
+    type: 'input',
+    message: 'What is your employee ID?',
+    name: 'intEmployeeID',
+    },
+    {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'intEmailAddress',
+    },
+    {
+    type: 'input',
+    message: 'What is your github user name?',
+    name: 'intSchool',
+    },
+    {
+    type: 'list',
+    message: 'Would you like to add any team members, or save your team as-is?',
+    name: 'addTeamMember',
+    choices: ['Engineer', 'Intern', 'Save Team'],
+    },
+  ])
+  // Once user prompts have been completed, ask the user if they're done creating their team, or if they want to add more memebers
+  .then((response) => {
+      // START TEST CODE
+      console.log("Intern added!");
+      console.log(`intName: ${response.intName} \nintEmployeeID: ${response.intEmployeeID} \nintEmailAddress: ${response.intEmailAddress} \nmgrintSchool: ${response.intSchool}`)
+      // END TEST CODE 
+      if (response.addTeamMember === 'Engineer') {
+        addEngineer();
+    } else if (response.addTeamMember === 'Intern') {
+        addIntern();
+    } else {
+      // Build HTML with objects created during inquirer prompts
+      // call buildHTML()
+    };
+
+  });
 };
 
 startTeam();
